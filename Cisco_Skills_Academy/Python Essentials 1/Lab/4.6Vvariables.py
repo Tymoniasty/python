a1f7b10ef76451a87f42337d9ab6c98fd7bd93b0
tym0niasty <tymon.tymoniasty@gmail.com>
2023-02-08 21:53:37 +0000

    sync

---
+++ Cisco_Skills_Academy/Python Essentials 1/Lab/4.6Vvariables.py
@@ -0,0 +1,7 @@
+john = 3
+mary = 5
+adam = 6
+print(john,",",mary,",",adam)
+print(john,mary,adam, sep=", ")
+total_apples=john+mary+adam
+print("Apples owned together by John, Mary and Adam",total_apples)
