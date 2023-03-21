ec4393b24c7b09448976d97909b7ec6703ebcad1
tym0niasty <tymon.tymoniasty@gmail.com>
2023-03-21 22:22:15 +0000

    while loop with else branch

---
+++ Cisco_Skills_Academy/Python Essentials 1/Lab/Boolean Values, Conditional Execution, Loops, Lists and List Processing, Logical and Bitwise Operations/2.8lab1.py
@@ -0,0 +1,25 @@
+'''
+Listen to this story: a boy and his father, a computer programmer, are playing with wooden blocks. 
+They are building a pyramid.
+Their pyramid is a bit weird, as it is actually a pyramid-shaped wall – it's flat. 
+The pyramid is stacked according to one simple principle: each lower layer contains one block more than the layer above.
+The figure illustrates the rule used by the builders:
+    #
+   ##
+  ###
+Your task is to write a program which reads the number of blocks the builders have, 
+and outputs the height of the pyramid that can be built using these blocks.
+Note: the height is measured by the number of fully completed layers – if the builders 
+don't have a sufficient number of blocks and cannot complete the next layer, they finish their work immediately.
+Test your code using the data we've provided.
+'''
+number_of_blocks = int(input("Please provide number of blocks: "))
+height = 0
+lower_level = 0
+while number_of_blocks > lower_level:
+  lower_level = height + 1
+  number_of_blocks-=lower_level
+  height += 1
+else:
+  print("The height of the piramid: ", height)
+
